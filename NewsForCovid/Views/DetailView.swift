4cb7819e68612aa9a01a7ef714a1d64b75f1fdaa
Amey Viney <amey@Ameys-Mac.local>
2020-08-04 03:27:46 -0700

    WebKit added

--- NewsForCovid/Views/DetailView.swift
+++ NewsForCovid/Views/DetailView.swift
@@ -7,7 +7,6 @@
 //
 
 import SwiftUI
-import WebKit
 
 struct DetailView: View {
     
@@ -19,23 +18,7 @@ struct DetailView: View {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(url: "https://github.com").previewDevice(PreviewDevice.init(rawValue: "iPhone 11"))
+        DetailView(url: "https://apple.com").previewDevice(PreviewDevice.init(rawValue: "iPhone 11"))
     }
 }
 
-struct WebView: UIViewRepresentable{
-    
-    let urlString: String?
-    
-    func makeUIView(context: Context) -> WebView.UIViewType {
-        return WKWebView()
-    }
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        if let safeString = urlString{
-            if let url = URL(string: safeString){
-                let request = URLRequest(url: url)
-                uiView.load(request)
-            }
-        }
-    }
-}
--- NewsForCovid/Views/WebView.swift
+++ NewsForCovid/Views/WebView.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable{
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView{
+        return WKWebView()
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString{
+            if let url = URL(string: safeString){
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
